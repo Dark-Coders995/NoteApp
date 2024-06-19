e45761afebb4044805b816201000fd9ea20b6166
Ayush Gupta <ag141020@gmail.com>
2024-06-19 23:32:23 +0530

    Change Deprecated Divider to Horizontal Divider

--- app/src/main/java/com/agworks/noteapp/screen/NoteScreen.kt
+++ app/src/main/java/com/agworks/noteapp/screen/NoteScreen.kt
@@ -9,7 +9,6 @@ import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.Notifications
-import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
